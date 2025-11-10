f42da16650954fddae5a5a0c7103e2e1f25a3dd6
fnd.davi <fernando.ferreira32@fatec.sp.gov.br>
2025-11-10 17:49:58 -0300

    add jest.config

---
+++ jest.config.cjs
@@ -0,0 +1,13 @@
+/**
+ * Jest config for TypeScript using ts-jest
+ */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testMatch: ['**/?(*.)+(spec|test).ts?(x)']
+};
